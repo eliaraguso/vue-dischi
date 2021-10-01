7a2de18af8bb6a3d1a8df7b885084bc1a3d858df
eliaraguso <elia.raguso@gmail.com>
2021-10-01 17:45:49 +0200

    fix problema chiamata axios e relativa rappresentazione dinamica degli album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-    name: "Component"
+    name: "Album"
 }
 </script>
 
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -23,7 +23,7 @@ export default {
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then( (response) => {
-                this.albums = response.data;
+                this.albums = response.data.response;
             });
     }
 }
